6077cc7018445fcdcbbc6ae4e93630435f2949dc
rejoan <rejoan@digitalpylot.io>
2025-08-21 08:41:55 +0600

    Update package dependencies and refactor layout for improved type handling
    - Added @types/js-cookie to devDependencies for better TypeScript support.
    - Updated RootLayout to handle params as a Promise for improved async handling.
    - Enhanced i18n configuration to ensure locale is explicitly typed as a string.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode
-  params: { locale: string }
+  params: Promise<{ locale: string }>
 }) {
   const { locale } = await params
   console.log('------>', locale)
--- lib/i18n.ts
+++ lib/i18n.ts
@@ -10,6 +10,7 @@ export default getRequestConfig(async ({ locale }) => {
   }
 
   return {
+    locale: locale as string,
     messages: (await import(`../messages/${locale}.json`)).default,
   }
 })
